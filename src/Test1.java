b6997900197364505af29308cd347c7ebf746dc0
sivaram <naidu.sivaram007@gmail.com>
2023-01-29 13:04:09 +0530

    First Commit.......

---
+++ src/Test1.java
@@ -0,0 +1,17 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s= "Yes";
+		if(s=="Yes")
+		System.out.println("eat something");
+		else {
+			System.out.println("be happy");
+		}
+		
+			
+
+	}
+
+}
